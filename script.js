4481fc7e6c19d6e429fc47cbe5fe844cd58737f6
tgorsk15 <tgbusiness463@gmail.com>
2023-11-04 12:44:50 -0500

    Build structure and psuedocode for project

---
+++ script.js
@@ -0,0 +1,24 @@
+// tic tac toe code structure:
+// want screen to open up with a dialogue box that you
+// have to click yes on to play
+
+// Once 'yes' has been clicked, another box appears (fades in)
+// below the first, prompting the user to put in the team names,
+// if they want
+
+// From here, user presses "Start game", making both dialogue boxes
+// dissapear, being replaced with the actual game board
+
+
+// Checking for a win: possibly have to test the array for
+// specific patterns of Xs or Os that would determine
+// if the game has been won
+// ** in a similar vein, once the length of the array is
+// 9, no more items can be added and the game ends in a Draw
+// after one last check for a win
+
+
+// factory function to create Player
+function createPlayer () {
+    
+}
