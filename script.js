8385641eba9645e734ba33366f9bddc50a9853b1
tgorsk15 <tgbusiness463@gmail.com>
2023-11-08 20:01:29 -0600

    Add eventlistener to the .box div containers
    Link box containers so user can interact with them when clicked. Will have to work on
    getting these event listeners connected to the game functions

--- script.js
+++ script.js
@@ -18,25 +18,28 @@
 // after one last check for a win
 
 
-const boardMarks = [];
 // IIFE for the game board
 const createBoard = (function () {
+    const boardMarks = [];
     const gameBoard = document.querySelector('.gameboard');
     const gameBoxes = gameBoard.getElementsByTagName('div');
     console.log(gameBoxes)
     for (let i = 0; i < gameBoxes.length; i++) {
         boardMarks.push(gameBoxes[i].innerHTML)
     };
+    console.log(boardMarks)
    
 })();
 
 
 // factory function for playing a round
-
+const playRound = function (player1, player2) {
+    console.log(player1);
+    console.log(player2);
+}
 
 // factory function to create Player
 const createPlayer = function (playerName, symbol) {
-    // const player1Score = document
     return {
         playerName: playerName,
         symbol: symbol,
@@ -45,11 +48,11 @@ const createPlayer = function (playerName, symbol) {
 
 
 //Event Listener for start game button
-const StartButton = document.querySelector('.start-game')
-StartButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    const player1Name = document.getElementById('player1').value
-    const player2Name = document.getElementById('player2').value
+const startButton = document.querySelector('.start-game')
+startButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const player1Name = document.getElementById('first-player').value
+    const player2Name = document.getElementById('second-player').value
     
     const player1 = createPlayer(player1Name, 'X');
     const player2 = createPlayer(player2Name, 'O');
@@ -58,12 +61,24 @@ StartButton.addEventListener('click', (e) => {
     console.log(player1);
     console.log(player2);
     
+    // link to playRound function
+    // playRound(player1, player2);
+
     // make container dissapear to make way for gameboard
     const promptsContainer = document.querySelector('.prompts-container')
 
     clearContainer(promptsContainer);
 
     
+})
+
+const boxesClicked = document.querySelectorAll('.box');
+// console.log(boxesClicked);
+boxesClicked.forEach(box => {
+    box.addEventListener('click', () => {
+        box.style.backgroundColor = 'black';
+        console.log('done')
+    })
 })
 
 // const test = createPlayers()
