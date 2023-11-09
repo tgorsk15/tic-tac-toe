9e575a1f296bbd2532c6f720f115125662e7fbe3
tgorsk15 <tgbusiness463@gmail.com>
2023-11-08 19:12:21 -0600

    Revamp createPlayer() and start-game addEventListener
    cleaned up the player factory function and renamed to createPlayer(). Also adjusted the
    addEventListener to match the variables in the new createPlayer(), and to feed in to the
    function correctly

--- script.js
+++ script.js
@@ -17,47 +17,46 @@
 // 9, no more items can be added and the game ends in a Draw
 // after one last check for a win
 
+
+const boardMarks = [];
 // IIFE for the game board
 const createBoard = (function () {
-    const boardMarks = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
     const gameBoard = document.querySelector('.gameboard');
-    const boardDivs = gameBoard.getElementsByTagName('div');
-    console.log(boardDivs.textContent)
-    for (let i = 0; i < boardMarks.length; i++) {
-        boardDivs[i].textContent = boardMarks[i];
+    const gameBoxes = gameBoard.getElementsByTagName('div');
+    console.log(gameBoxes)
+    for (let i = 0; i < gameBoxes.length; i++) {
+        boardMarks.push(gameBoxes[i].innerHTML)
     };
    
 })();
 
 
+// factory function for playing a round
+
+
 // factory function to create Player
-const createPlayers = (function () {
+const createPlayer = function (playerName, symbol) {
     // const player1Score = document
-    const returnPlayer = (player1Name, player2Name) => {
-        const player1 = player1Name;
-        const player2 = player2Name;
-        console.log('Player1: ', player1 )
-        console.log('Player2: ', player2 )
-    };
-
-    // const returnScore = () => {
-
-    // }
-    return returnPlayer;
-
-})();
+    return {
+        playerName: playerName,
+        symbol: symbol,
+    }
+};
 
 
 //Event Listener for start game button
 const StartButton = document.querySelector('.start-game')
 StartButton.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const player1Name = document.getElementById('player1').value
     const player2Name = document.getElementById('player2').value
-    console.log(player1Name)
-    console.log(player2Name)
-    createPlayers(player1Name, player2Name);
+    
+    const player1 = createPlayer(player1Name, 'X');
+    const player2 = createPlayer(player2Name, 'O');
+
     console.log('done')
+    console.log(player1);
+    console.log(player2);
     
     // make container dissapear to make way for gameboard
     const promptsContainer = document.querySelector('.prompts-container')
